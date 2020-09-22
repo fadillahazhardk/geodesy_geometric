013218b3fe80a7bdf18fbb87950797315b5b2240
fadillahazhardk <fadillahazhardk@gmail.com>
2020-09-22 17:37:00 +0700

    add data di surfaceAreaOnEllipsoid

--- calculateSurfaceAreaOnEllipsoid.js
+++ calculateSurfaceAreaOnEllipsoid.js
@@ -1,8 +1,7 @@
 const math = require("mathjs");
 var convert = require("convert-units");
 
-//Data
-// max val = 60
+//Konversi degree, minute to degree decimal
 const dmToDeg = (val) => {
   const degCoef = 1 / 60;
   const min = val - math.floor(val);
@@ -15,6 +14,7 @@ const a = 6378137; //meter
 const b = 6356752.3142; //meter
 const e = Math.sqrt(math.pow(a, 2) - math.pow(b, 2)) / a;
 
+//Perhitungan
 const calcMeridianRad = (lat) => {
   const pembilang = a * (1 - math.pow(e, 2));
   const penyebut = math.pow(
@@ -31,7 +31,6 @@ const calcIrisanVerUtamaRad = (lat) => {
   );
   return pembilang / penyebut;
 };
-
 const calcEllipsoidSurface = (arr) => {
   let L = null;
   arr.forEach((obj) => {
@@ -52,21 +51,84 @@ const calcEllipsoidSurface = (arr) => {
 
     L += Mi * Ni * math.cos(math.unit(cntrLat, "deg")) * difLat * difLon;
   });
-  console.log(L);
   return L;
 };
 
+// DATA GRID
+const data = [
+  { lat1: 6.51, lat2: 6.52, lon1: 107.35, lon2: 107.36 },
+  { lat1: 6.52, lat2: 6.53, lon1: 107.35, lon2: 107.36 },
+  { lat1: 6.52, lat2: 6.53, lon1: 107.36, lon2: 107.37 },
+  { lat1: 6.52, lat2: 6.53, lon1: 107.37, lon2: 107.38 },
+  { lat1: 6.53, lat2: 6.54, lon1: 107.35, lon2: 107.36 },
+  { lat1: 6.53, lat2: 6.54, lon1: 107.36, lon2: 107.37 },
+  { lat1: 6.53, lat2: 6.54, lon1: 107.37, lon2: 107.38 },
+  { lat1: 6.54, lat2: 6.55, lon1: 107.34, lon2: 107.35 },
+  { lat1: 6.54, lat2: 6.55, lon1: 107.35, lon2: 107.36 },
+  { lat1: 6.54, lat2: 6.55, lon1: 107.36, lon2: 107.37 },
+  { lat1: 6.54, lat2: 6.55, lon1: 107.37, lon2: 107.38 },
+  { lat1: 6.54, lat2: 6.55, lon1: 107.38, lon2: 107.39 },
+  { lat1: 6.54, lat2: 6.55, lon1: 107.39, lon2: 107.4 },
+  { lat1: 6.54, lat2: 6.55, lon1: 107.4, lon2: 107.41 },
+  { lat1: 6.54, lat2: 6.55, lon1: 107.41, lon2: 107.42 },
+  { lat1: 6.54, lat2: 6.55, lon1: 107.42, lon2: 107.43 },
+  { lat1: 6.54, lat2: 6.55, lon1: 107.43, lon2: 107.44 },
+  { lat1: 6.55, lat2: 6.56, lon1: 107.34, lon2: 107.35 },
+  { lat1: 6.55, lat2: 6.56, lon1: 107.35, lon2: 107.36 },
+  { lat1: 6.55, lat2: 6.56, lon1: 107.37, lon2: 107.38 },
+  { lat1: 6.55, lat2: 6.56, lon1: 107.38, lon2: 107.39 },
+  { lat1: 6.55, lat2: 6.56, lon1: 107.39, lon2: 107.4 },
+  { lat1: 6.55, lat2: 6.56, lon1: 107.4, lon2: 107.41 },
+  { lat1: 6.55, lat2: 6.56, lon1: 107.41, lon2: 107.42 },
+  { lat1: 6.55, lat2: 6.56, lon1: 107.42, lon2: 107.43 },
+  { lat1: 6.56, lat2: 6.57, lon1: 107.34, lon2: 107.35 },
+  { lat1: 6.56, lat2: 6.57, lon1: 107.35, lon2: 107.36 },
+  { lat1: 6.56, lat2: 6.57, lon1: 107.36, lon2: 107.37 },
+  { lat1: 6.56, lat2: 6.57, lon1: 107.37, lon2: 107.38 },
+  { lat1: 6.56, lat2: 6.57, lon1: 107.38, lon2: 107.39 },
+  { lat1: 6.56, lat2: 6.57, lon1: 107.39, lon2: 107.4 },
+  { lat1: 6.56, lat2: 6.57, lon1: 107.4, lon2: 107.41 },
+  { lat1: 6.57, lat2: 6.58, lon1: 107.38, lon2: 107.39 },
+  { lat1: 6.57, lat2: 6.58, lon1: 107.39, lon2: 107.4 },
+  { lat1: 6.57, lat2: 6.58, lon1: 107.4, lon2: 107.41 },
+  { lat1: 6.57, lat2: 6.58, lon1: 107.35, lon2: 107.36 },
+  { lat1: 6.57, lat2: 6.58, lon1: 107.36, lon2: 107.37 },
+  { lat1: 6.57, lat2: 6.58, lon1: 107.37, lon2: 107.38 },
+  { lat1: 6.53, lat2: 6.54, lon1: 107.34, lon2: 107.35 },
+  { lat1: 6.56, lat2: 6.57, lon1: 107.41, lon2: 107.42 },
+  { lat1: 6.56, lat2: 6.57, lon1: 107.42, lon2: 107.43 },
+];
+const dataSetengah = [
+  { lat1: 6.52, lat2: 6.53, lon1: 107.34, lon2: 107.35 },
+  { lat1: 6.53, lat2: 6.54, lon1: 107.38, lon2: 107.39 },
+  { lat1: 6.53, lat2: 6.54, lon1: 107.39, lon2: 107.4 },
+  { lat1: 6.53, lat2: 6.54, lon1: 107.4, lon2: 107.41 },
+  { lat1: 6.54, lat2: 6.55, lon1: 107.34, lon2: 107.35 },
+  { lat1: 6.55, lat2: 6.56, lon1: 107.43, lon2: 107.44 },
+  { lat1: 6.56, lat2: 6.57, lon1: 107.33, lon2: 107.34 },
+  { lat1: 6.57, lat2: 6.58, lon1: 107.41, lon2: 107.42 },
+  { lat1: 6.5, lat2: 6.51, lon1: 107.35, lon2: 107.36 },
+  { lat1: 6.51, lat2: 6.52, lon1: 107.34, lon2: 107.35 },
+  { lat1: 6.53, lat2: 6.54, lon1: 107.33, lon2: 107.34 },
+  { lat1: 6.53, lat2: 6.54, lon1: 107.41, lon2: 107.42 },
+  { lat1: 6.53, lat2: 6.54, lon1: 107.42, lon2: 107.43 },
+  { lat1: 6.53, lat2: 6.54, lon1: 107.43, lon2: 107.44 },
+  { lat1: 6.53, lat2: 6.54, lon1: 107.44, lon2: 107.45 },
+  { lat1: 6.54, lat2: 6.55, lon1: 107.33, lon2: 107.34 },
+  { lat1: 6.54, lat2: 6.55, lon1: 107.44, lon2: 107.45 },
+  { lat1: 6.55, lat2: 6.56, lon1: 107.33, lon2: 107.34 },
+  { lat1: 6.56, lat2: 6.57, lon1: 107.43, lon2: 107.44 },
+  { lat1: 6.57, lat2: 6.58, lon1: 107.33, lon2: 107.34 },
+  { lat1: 6.57, lat2: 6.58, lon1: 107.34, lon2: 107.35 },
+];
+const fullArea = calcEllipsoidSurface(data);
+const halfArea = calcEllipsoidSurface(dataSetengah) * 0.5;
+const totalArea = fullArea + halfArea;
+
+//PrintOut
+console.log("luas wilayah bandung adalah: " + totalArea + " m2");
 console.log(
-  convert(
-    calcEllipsoidSurface([
-      {
-        lat1: 6.54,
-        lat2: 6.55,
-        lon1: 107.38,
-        lon2: 107.39,
-      },
-    ])
-  )
-    .from("m2")
-    .to("km2")
+  "jika dinyatakan dalam kilometer: " +
+    convert(totalArea).from("m2").to("km2") +
+    " km2"
 );
