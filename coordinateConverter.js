216fa0bdea5f6892bb028547a723944291c59b2b
fadillahazhardk <fadillahazhardk@gmail.com>
2020-10-05 21:34:52 +0700

    coordinate converter 2.0

--- coordinateConverter.js
+++ coordinateConverter.js
@@ -9,21 +9,21 @@ const { atan, cos } = require("mathjs");
 //Metode tertutup dari geodetik ke kartesia3D
 exports.geoToKartClosed = (h, lat, lon, ellipsoid) => {
   let a = null,
-    b = null,
-    N = null,
-    e = null;
+    b = null;
 
   if (ellipsoid === "wgs84") {
     a = 6378137; //meter
     b = 6356752.3142; //meter
-    e = Math.sqrt(math.pow(a, 2) - math.pow(b, 2)) / a;
-    N = calcRad.calcIrisanVerUtamaRad(lat, a, e);
   } else if (ellipsoid === "bessel") {
     a = 6377397.155;
     b = 6356078.963;
-    e = Math.sqrt(math.pow(a, 2) - math.pow(b, 2)) / a;
-    N = calcRad.calcIrisanVerUtamaRad(lat, a, e);
+  } else if (ellipsoid === "grs80") {
+    a = 6378137;
+    b = 6356752.314140347;
   }
+
+  const e = Math.sqrt(math.pow(a, 2) - math.pow(b, 2)) / a;
+  const N = calcRad.calcIrisanVerUtamaRad(lat, a, e);
 
   const x =
       (N + h) *
@@ -48,31 +48,25 @@ exports.geoToKartClosed = (h, lat, lon, ellipsoid) => {
 exports.kartToGeoClosed = (X, Y, Z, ellipsoid) => {
   //Data
   let a = null,
-    b = null,
-    e1 = null,
-    e2 = null,
-    theta = null;
-  const p = math.sqrt(X ** 2 + Y ** 2);
-
+    b = null;
   if (ellipsoid === "wgs84") {
     a = 6378137; //meter
     b = 6356752.3142; //meter
-    e1 = Math.sqrt(math.pow(a, 2) - math.pow(b, 2)) / a;
-    e2 = Math.sqrt(math.pow(a, 2) - math.pow(b, 2)) / b;
-    theta = convert(math.atan2(Z * a, p * b))
-      .from("rad")
-      .to("deg");
   } else if (ellipsoid === "bessel") {
     a = 6377397.155;
     b = 6356078.963;
-    e1 = Math.sqrt(math.pow(a, 2) - math.pow(b, 2)) / a;
-    e2 = Math.sqrt(math.pow(a, 2) - math.pow(b, 2)) / b;
-    theta = convert(math.atan2(Z * a, p * b))
-      .from("rad")
-      .to("deg");
+  } else if (ellipsoid === "grs80") {
+    a = 6378137;
+    b = 6356752.314140347;
   }
+  const p = math.sqrt(X ** 2 + Y ** 2);
+  const e1 = Math.sqrt(math.pow(a, 2) - math.pow(b, 2)) / a;
+  const e2 = Math.sqrt(math.pow(a, 2) - math.pow(b, 2)) / b;
+  const theta = convert(math.atan2(Z * a, p * b))
+    .from("rad")
+    .to("deg");
 
-  //Calculate
+  //Calculate Coord
   const lon = convert(math.atan2(Y, X)).from("rad").to("deg"); //we got radian unit
 
   const pembilangLat =
@@ -84,7 +78,6 @@ exports.kartToGeoClosed = (X, Y, Z, ellipsoid) => {
     .to("deg");
 
   const N = calcRad.calcIrisanVerUtamaRad(lat, a, e1);
-
   const h = p / math.cos(math.unit(lat, "deg")) - N;
 
   return {
@@ -103,22 +96,25 @@ exports.kartToGeoClosed = (X, Y, Z, ellipsoid) => {
 //Metode iteratif dari kartesian3D ke geodetik
 exports.kartToGeoIteratif = (X, Y, Z, ellipsoid) => {
   let a = null,
-    b = null,
-    e = null;
+    b = null;
 
   if (ellipsoid === "wgs84") {
     a = 6378137; //meter
     b = 6356752.3142; //meter
-    e = Math.sqrt(math.pow(a, 2) - math.pow(b, 2)) / a;
   } else if (ellipsoid === "bessel") {
     a = 6377397.155;
     b = 6356078.963;
-    e = Math.sqrt(math.pow(a, 2) - math.pow(b, 2)) / a;
+  } else if (ellipsoid === "grs80") {
+    a = 6378137;
+    b = 6356752.314140347;
+    // f =  0.003 352 810 681 183 637 418;
   }
 
+  const e = Math.sqrt(math.pow(a, 2) - math.pow(b, 2)) / a;
   const p = math.sqrt(X ** 2 + Y ** 2);
 
   let h = 0,
+    hSebelum,
     N = 1,
     lat = 0,
     latSebelum = 0;
@@ -130,14 +126,15 @@ exports.kartToGeoIteratif = (X, Y, Z, ellipsoid) => {
         .to("deg");
     N = calcRad.calcIrisanVerUtamaRad(lat, a, e);
     h = p / math.cos(math.unit(lat, "deg")) - N;
-    if (latSebelum === lat) break
-    latSebelum = lat
+    if (latSebelum === lat && hSebelum === h) break;
+    hSebelum = h;
+    latSebelum = lat;
   }
   const lon = convert(math.atan2(Y, X)).from("rad").to("deg");
-  
+
   return {
     message: `koordinatnya: ${latSebelum}, ${lon}, ${h}`,
-    lat = latSebelum,
+    lat: latSebelum,
     lon,
     h,
   };
--- index.js
+++ index.js
@@ -1,36 +1,106 @@
 const corconv = require("./coordinateConverter");
-const calc = require("./calculateSurfaceAreaOnEllipsoid");
-const math = require("mathjs");
-const convert = require("convert-units");
 
-// //WGS 84
-const a = 6378137; //meter
-// const f = 1/298.257223563
-const b = 6356752.3142; //meter
-const e = Math.sqrt(math.pow(a, 2) - math.pow(b, 2)) / a;
+//Soal Nomor 1
+//Koordinat Titik Q di sistem koordinat geodetik dengan referensi WGS84
+// const lat = -6.57995799; //deg
+// const lon = 107.875574; //deg
+// const h = 734.881; //meter
 
-// //Geo Coordinate
-// const lat = 1;
-// const lon = 1;
-// const h = 1;
+// //Koordinat titik Q di kartesian3D
+// const kartcoord = corconv.geoToKartClosed(h, lat, lon, "wgs84");
+// const X = kartcoord.x;
+// const Y = kartcoord.y;
+// const Z = kartcoord.z;
 
-// //Kartesian Coordinate
-// const X = 1;
-// const Y = 1;
-// const Z = 1;
+// //Koordinat Titik Q di sistem koordinat geodetik dengan referensi Bessel 1841
+// const coordOnBessel = corconv.kartToGeoClosed(X, Y, Z, "bessel");
+// const latBessel = coordOnBessel.lat;
+// const lonBessel = coordOnBessel.lon;
+// const hBessel = coordOnBessel.h;
 
-// const N = calcRad.calcIrisanVerUtamaRad(lat);
-// const M = calcRad.calcMeridianRad(lat);
+// const coordOnBessel2 = corconv.kartToGeoIteratif(X, Y, Z, "bessel");
+// const latBessel2 = coordOnBessel2.lat;
+// const lonBessel2 = coordOnBessel2.lon;
+// const hBessel2 = coordOnBessel2.h;
 
-// console.log(corconv.kartToGeoClosed(1,1,0, "bessel"))
+// console.log(`
+//     SOAL NOMOR SATU
+//     Koordinat titik Q di WGS84: lat = ${lat}; lon = ${lon}; h = ${h}.
 
-//Data
-const lat = -6.57995799; //deg
-const lon = 107.875574; //deg
+//     1. Pendekatan dengan formula tertutup
+//     Koodinat titik Q di Bessel1841: lat = ${latBessel}; lon = ${lonBessel}; h = ${hBessel}.
+
+//     2. Pendekatan dengan formula iteratif
+//     Koodinat titik Q di Bessel1841: lat = ${latBessel2}; lon = ${lonBessel2}; h = ${hBessel2}.
+// `);
+
+// Soal Nomor 2
+// Koordinat Titik Q di sistem koordinat geodetik dengan referensi GRS80
+const lat = -6.579958; //deg
+const lon = 107.8756; //deg
 const h = 734.881; //meter
-const kartcoord = corconv.geoToKartClosed(h, lat, lon, "wgs84");
-const X = kartcoord.x;
-const Y = kartcoord.y;
-const Z = kartcoord.z;
 
-console.log(corconv.kartToGeoIteratif(X, Y, Z, "bessel"));
+const kartcoord = corconv.geoToKartClosed(h, lat, lon, "grs80");
+const X = kartcoord.x + 1;
+const Y = kartcoord.y - 2;
+const Z = kartcoord.z + 2;
+
+const coordOnGRS80 = corconv.kartToGeoClosed(X, Y, Z, "grs80");
+const lat1 = coordOnGRS80.lat;
+const lon1 = coordOnGRS80.lon;
+const h1 = coordOnGRS80.h;
+
+const coordOnGRS802 = corconv.kartToGeoIteratif(X, Y, Z, "grs80");
+const lat2 = coordOnGRS802.lat;
+const lon2 = coordOnGRS802.lon;
+const h2 = coordOnGRS802.h;
+
+console.log(`
+    SOAL NOMOR DUA
+
+    Koordinat titik Q di GRS80: lat = ${lat}; lon = ${lon}; h = ${h}.
+    Koordinat titik Q di Kartesian3D pertama: X = ${X - 1}; Y = ${Y + 2}; Z = ${
+  Z - 2
+}.  
+    Koordinat titik Q di Kartesian3D kedua: X = ${X}; Y = ${Y}; Z = ${Z}.
+
+    1. Pendekatan dengan cara formula tertutup
+    Koodinat titik Q di GRS80: lat = ${lat1}; lon = ${lon1}; h = ${h1}.
+
+    2. Pendekatan dengan cara formula iteratif
+    Koodinat titik Q di GRS80: lat = ${lat2}; lon = ${lon2}; h = ${h2}.
+`);
+
+// Soal Nomor 3
+// Koordinat Titik P di sistem koordinat geodetik dengan referensi WGS84
+// const lat = -6.579958;
+// const lon = 107.8756;
+// const h = 734.881;
+
+// const kartcoord = corconv.geoToKartClosed(h, lat, lon, "wgs84");
+// const X = kartcoord.x - 1577.302;
+// const Y = kartcoord.y + 32.484;
+// const Z = kartcoord.z + 53.056;
+
+// const coordOnWGS = corconv.kartToGeoClosed(X, Y, Z, "wgs84");
+// const latQ = coordOnWGS.lat;
+// const lonQ = coordOnWGS.lon;
+// const hQ = coordOnWGS.h;
+
+// const coordOnWGS2 = corconv.kartToGeoIteratif(X, Y, Z, "wgs84");
+// const latQ2 = coordOnWGS2.lat;
+// const lonQ2 = coordOnWGS2.lon;
+// const hQ2 = coordOnWGS2.h;
+
+// console.log(`
+//     SOAL NOMOR TIGA
+//     Koordinat titik P di GWS84: lat = ${lat}; lon = ${lon}; h = ${h}.
+
+//     1. Pendekatan Closed
+//     Koodinat titik P di WGS84: lat = ${latQ}; lon = ${lonQ}; h = ${hQ}.
+//     Koordinat titik P di Kartesian3D: X = ${X - 1577.302}; Y = ${Y + 32.484}; Z = ${Z - 53.056}.
+
+//     2. Pendekatan Iteratif
+//     Koodinat titik Q di WGS84: lat = ${latQ2}; lon = ${lonQ2}; h = ${hQ2}.
+//     Koordinat titik Q di Kartesian3D kedua: X = ${X}; Y = ${Y}; Z = ${Z}.
+// `);
